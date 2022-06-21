0751077a47619f9792775c2c38a3c9aa8215469d
chinkhuslen <chinkhuslen10@gmail.com>
2022-06-21 11:41:58 +0800

    profile nemlee

---
+++ profile/app.js
@@ -0,0 +1,47 @@
+let TuruuAnimatiom=new TimelineLite();
+
+     TuruuAnimatiom
+     .fromTo(
+          "#tomMod" ,  8 , {
+               width: 0  + "%" , height:0  + "%" , bottom: 5 + "vh" 
+          } , {
+               width: 100 +"%", height: 90 + "%" ,bottom: 5 + "vh" 
+          } , "-=1"
+     )
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
